74ff03328ab64be110f89cb0a996885c3737e8a7
kevin hart <me@itsman.org>
2023-05-18 11:58:31 +0800

    添加network tools module

---
+++ network/src/main/java/org/itsman/network/QuoteList.kt
@@ -0,0 +1,21 @@
+package org.itsman.network
+
+data class QuoteList(
+    val count: Int,
+    val lastItemIndex: Int,
+    val page: Int,
+    val results: List<Result>,
+    val totalCount: Int,
+    val totalPages: Int
+)
+
+data class Result(
+    val _id: String,
+    val author: String,
+    val authorSlug: String,
+    val content: String,
+    val dateAdded: String,
+    val dateModified: String,
+    val length: Int,
+    val tags: List<String>
+)
