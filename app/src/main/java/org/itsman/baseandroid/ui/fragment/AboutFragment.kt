9e7ac0769f128f069423b183942a13b1aeac82eb
kevin hart <me@itsman.org>
2023-07-19 15:03:12 +0800

    update

--- app/src/main/java/org/itsman/baseandroid/ui/fragment/AboutFragment.kt
+++ app/src/main/java/org/itsman/baseandroid/ui/fragment/AboutFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import org.itsman.baseandroid.R
 
 class AboutFragment : Fragment() {
@@ -20,13 +24,16 @@ class AboutFragment : Fragment() {
     ): View? {
         return ComposeView(requireContext()).apply {
             setContent {
-                init()
+                init(Navigation.findNavController(this))
             }
         }
     }
 }
 
 @Composable
-private fun init() {
-    Image(painter = painterResource(id = R.drawable.asdf), contentDescription = null)
+private fun init(findNavController: NavController) {
+    Image(painter = painterResource(id = R.drawable.asdf),
+        contentDescription = null,
+        modifier = Modifier.clickable { findNavController.navigateUp() }
+    )
 }
