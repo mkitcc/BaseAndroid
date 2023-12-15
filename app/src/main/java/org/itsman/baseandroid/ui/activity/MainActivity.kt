4924f605b42c324db27a855f9dbec71172bd5c2a
kevin hart <me@itsman.org>
2023-12-15 18:01:05 +0800

    update

--- app/src/main/java/org/itsman/baseandroid/ui/activity/MainActivity.kt
+++ app/src/main/java/org/itsman/baseandroid/ui/activity/MainActivity.kt
@@ -2,38 +2,51 @@ package org.itsman.baseandroid.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import org.itsman.baseandroid.databinding.ActivityMainBinding
+import org.itsman.baseandroid.ui.compose.Counter
+import org.itsman.baseandroid.ui.compose.Show
 import org.itsman.baseandroid.viewmodel.MainActivityVM
 import org.itsman.fastlibrary.dialog.CustomDialog
+import org.itsman.fastlibrary.theme.BaseAndroidTheme
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 class MainActivity : BaseActivity() {
 
-    private val model: MainActivityVM by viewModels()
-    private var aa:String by MyTest()
-    private lateinit var bind: ActivityMainBinding
+    //    private val model: MainActivityVM by viewModels()
+//    private var aa: String by MyTest()
+//    private lateinit var bind: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bind = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bind.root)
-        WindowCompat.getInsetsController(window, window.decorView)
-            .hide(WindowInsetsCompat.Type.systemBars())
-        bind.button.text = "hello 123"
-        model.data.observe(this) {
-            bind.tvText.text = it
-            CustomDialog().show(supportFragmentManager)
+//        bind = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(bind.root)
+        setContent {
+            BaseAndroidTheme {
+                Show(name = "kevin")
+            }
         }
-        model.getData()
+
+//        WindowCompat.getInsetsController(window, window.decorView)
+//            .hide(WindowInsetsCompat.Type.systemBars())
+//        bind.button.text = "hello 123"
+//        model.data.observe(this) {
+//            bind.tvText.text = it
+//            CustomDialog().show(supportFragmentManager)
+//        }
+//        model.getData()
+//        bind.button.setOnClickListener {
+//            CustomDialog().show(supportFragmentManager)
+//        }
     }
 }
 
 
-class MyTest:ReadWriteProperty<MainActivity,String>{
+class MyTest : ReadWriteProperty<MainActivity, String> {
     override fun getValue(thisRef: MainActivity, property: KProperty<*>): String {
         return "Hello"
     }
--- app/src/main/java/org/itsman/baseandroid/ui/compose/MainCompose.kt
+++ app/src/main/java/org/itsman/baseandroid/ui/compose/MainCompose.kt
@@ -1,18 +1,24 @@
 package org.itsman.baseandroid.ui.compose
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,27 +29,51 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import org.itsman.baseandroid.R
+import org.itsman.fastlibrary.tools.toast
 
+@Preview(showBackground = true, name = "Main", showSystemUi = true)
 @Composable
-fun Show(name: String, list:List<String>): Unit {
+fun Show(name: String = "kevin"): Unit {
+    var data by remember {
+        mutableStateOf(1)
+    }
     val params = Modifier
-        .padding(22.dp)
+        .padding(10.dp)
+        .fillMaxWidth(1f)
         .background(color = Color.Green)
-    LazyColumn(
-        params,
-    ) {
-        for (i in 0..10){
-            item {
-                item(name = "Compose课程第${i+1}课,快来学习吧～")
-            }
+    Column {
+        Box(modifier = params) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth(1f)
+                    .clickable { data++ },
+                painter = painterResource(id = R.drawable.asdf),
+                contentDescription = null
+            )
+        }
+        Row {
+            Text(text = name + data,
+                color = Color(android.graphics.Color.parseColor("#009900")),
+                fontSize = TextUnit.Unspecified,
+                modifier = Modifier.size(140.dp)
+            )
+            Text(
+                text = "asdfasdfasdfweijasldfijweija;sldfijweijsdfkjfiesdkfjeiajsdlfiwejiasdfe",
+                overflow = TextOverflow.Visible,
+                maxLines = 1
+            )
         }
     }
+
 }
 
 @Composable
-fun item(name: String) {
+fun Item(name: String) {
     Row(
         Modifier
             .padding(10.dp)
@@ -52,20 +82,21 @@ fun item(name: String) {
         Text(text = name, textAlign = TextAlign.End)
         Image(
             painter = painterResource(id = R.drawable.asdf),
-            contentDescription = null
+            contentDescription = null,
+            Modifier.clickable { },
         )
     }
     Divider(color = Color.Black)
 }
 
 @Composable
-fun Counter(){
+fun Counter(number: Int = 0) {
     var number by remember {
-        mutableStateOf(0)
+        mutableStateOf(number)
     }
     Column {
         Text(text = "当前值:$number")
-        Button(onClick = {number++}) {
+        Button(onClick = { number++ }) {
             Text(text = "add")
         }
     }
