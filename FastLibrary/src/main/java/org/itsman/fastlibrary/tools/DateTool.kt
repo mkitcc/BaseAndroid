e255450463d65ccd5d3503df1c92c154914ed126
songjianyu <songjianyu@linkingplay.com>
2025-05-14 18:33:11 +0800

    update

---
+++ FastLibrary/src/main/java/org/itsman/fastlibrary/tools/DateTool.kt
@@ -0,0 +1,15 @@
+package org.itsman.fastlibrary.tools
+
+import android.text.format.Time
+import java.util.Calendar
+import java.util.TimeZone
+import java.util.Timer
+
+
+/**
+ * params = 时间戳
+ * return = MM/dd/yy
+ */
+fun transformDate(){
+
+}
